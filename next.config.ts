aeb4a3cc2ecfcb3270aa2039fed4ed759b0563f5
Oğuzhan Gümüş <oguzhaangumuss@hotmail.com>
2025-02-21 19:31:06 +0300

    Configure webpack to transpile Spline runtime package

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.(js|jsx)$/,
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-react'],
-      },
-    });
-    return config;
-  }
+  transpilePackages: ['@splinetool/runtime']
 };
 
 export default nextConfig;
